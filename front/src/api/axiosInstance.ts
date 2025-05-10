19cb217c5b1fa610ab9e99c7a7978a86345cca69
ksw6823 <ksw6823@naver.com>
2025-05-10 19:18:23 +0900

    API 호출 URL 경로 수정
    .env파일 URL만 수정하면 적용되도록 로직 수정

--- front/src/api/axiosInstance.ts
+++ front/src/api/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_BASE_URL } from './config';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
